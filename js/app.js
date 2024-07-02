d1535e91a828c43ca3628825751af1cafb31e3a2
Samuel Elias <samuelfelias2008@gmail.com>
2024-07-02 15:23:53 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+qntdPista = document.getElementById("qtd-pista").textContent
+qntdSuperior = document.getElementById("qtd-superior").textContent
+qntdInferior = document.getElementById("qtd-inferior").textContent
+function comprar(){
+    
+    qntdDesejada = document.getElementById("qtd").value
+    opc = document.getElementById("tipo-ingresso").value
+
+
+    if(opc == "inferior"){
+        if(qntdDesejada > qntdInferior){
+            alert("Quantidade indisponível para tipo inferior")
+        }else{
+            qntdInferior = qntdInferior - qntdDesejada
+            let spanInferior = document.getElementById("qtd-inferior")
+            spanInferior.innerHTML = `<span id="qtd-pista">${qntdInferior}</span>`
+        }
+    }
+    if(opc == "superior"){
+        if(qntdDesejada > qntdSuperior){
+            alert("Quantidade indisponível para tipo superior")
+        }else{
+            qntdSuperior = qntdSuperior - qntdDesejada
+            let spanSuperior = document.getElementById("qtd-superior")
+            spanSuperior.innerHTML = `<span id="qtd-pista">${qntdSuperior}</span>`
+        }
+    }
+    if(opc == "pista"){
+        if(qntdDesejada > qntdPista){
+            alert("Quantidade indisponível para tipo pista")
+        }else{
+            qntdPista = qntdPista - qntdDesejada
+            let spanPista = document.getElementById("qtd-pista")
+            spanPista.innerHTML = `<span id="qtd-pista">${qntdPista}</span>`
+        }
+    }
+    document.getElementById("qtd").value = ""
+}
+
+function imprimirInferior(){
+    
+}
